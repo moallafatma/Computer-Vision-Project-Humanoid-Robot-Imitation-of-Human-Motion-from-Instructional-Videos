f3f73a4289b204e3bde5300ccb0cb4ea95635e78
Fatma Moalla <54987951+moallafatma@users.noreply.github.com>
2020-01-05 18:47:46 +0100

    Update config.py
    deleted '..' from model_dir

--- src/config.py
+++ src/config.py
@@ -19,7 +19,7 @@ import json
 import numpy as np
 
 curr_path = osp.dirname(osp.abspath(__file__))
-model_dir = osp.join(curr_path, '..', 'models')
+model_dir = osp.join(curr_path,'models')
 #if not osp.exists(model_dir):
  #   print('Fix path to models/')
   #  import ipdb
