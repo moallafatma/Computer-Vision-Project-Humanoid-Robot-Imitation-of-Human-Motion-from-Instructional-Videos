035780ed67912fb413949feec645cd42832e6d0d
Fatma Moalla <54987951+moallafatma@users.noreply.github.com>
2020-01-05 21:12:39 +0100

    Update run_openpose.py

--- run_openpose.py
+++ run_openpose.py
@@ -501,7 +501,8 @@ def nonmaxsupp(bboxes0, valid_kps0):
         w = np.maximum(0, xx2 - xx1 + 1)
         h = np.maximum(0, yy2 - yy1 + 1)
         # compute the ratio of overlap
-	overlap = (w * h)/area[idxs[:last]]
+        print(area[idxs[:last]])
+	overlap = (w * h)/ area[idxs[:last]]
         # delete all indexes from the index list that have
 	idxs = np.delete(idxs, np.concatenate(([last],np.where(overlap > NMS_THR)[0])))
 
